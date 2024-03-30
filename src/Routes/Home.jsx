421898face8b1f9bfe916996e8237dd7394be0ae
Jeremias Gomez <jeregomezgiglio12@gmail.com>
2024-03-30 11:01:23 -0300

    rename project

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Card from "../Components/Card";
 import { useGlobalContext } from "../Components/utils/global.context";
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
   const { state } = useGlobalContext();
